a27c38871d03052b61ebd9935a70f308fbb21db3
261406-Khushbu Majithia <80452555+Khushbu-Majithia-261406@users.noreply.github.com>
2021-04-27 18:49:47 +0530

    Update sum.c

--- Implementation/src/sum.c
+++ Implementation/src/sum.c
@@ -1,1 +1,20 @@
+/**
+* Calculates the sum of 2 nos
+* @param [in,in] 2 numbers whose summation has to be done
+* @return summation of the 2 nos
+*/
+
+int sum(int number1, int number2);
+
+#endif  /* #ifndef __SUM_H__  */
+
+
+#include<stdio.h>
+
+/*return sum of the 2 nos*/
+
+int sum(int number1, int number2)
+{
+    return(number1+number2);
+}
 
