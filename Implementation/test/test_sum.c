209199014ba1f466292be4156531bdf5bfa9bace
261406-Khushbu Majithia <80452555+Khushbu-Majithia-261406@users.noreply.github.com>
2021-04-27 18:50:15 +0530

    Update test_sum.c

--- Implementation/test/test_sum.c
+++ Implementation/test/test_sum.c
@@ -1,1 +1,51 @@
 
+#include "unity.h"
+#include "sum.h"
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+void test_sum1(void)
+{
+  TEST_ASSERT_EQUAL(11, sum(5,6));
+}
+
+void test_sum2(void)
+{
+  TEST_ASSERT_EQUAL(4, sum(5,-1));
+}
+
+void test_sum3(void)
+{
+  TEST_ASSERT_EQUAL(4, sum(-1,5));
+}
+
+void test_sum4(void)
+{ 
+   TEST_ASSERT_EQUAL(-3, sum(-5,2));
+}
+
+void test_sum5(void)
+{
+    TEST_ASSERT_EQUAL(-3, sum(2,-5));
+}
+
+
+int test_main(void)
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_sum1);
+  RUN_TEST(test_sum2);
+  RUN_TEST(test_sum3);
+  RUN_TEST(test_sum4);
+  RUN_TEST(test_sum5);
+ 
+
+
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
